e45728bb22053a403dbc07b14dbee822a1a7f8fc
Christian Pfitzner <christian.pfitzner@evocortex.com>
2020-10-23 10:31:20 +0200

    Update and cleanup of github examples for c

--- 00_basic_operations/hello_world.c
+++ 00_basic_operations/hello_world.c
@@ -5,9 +5,21 @@
 #include <stdio.h>                // include for printf function
 
 
-int main(void)                    // start of the main programm          
+int main(void)                    // start of the main programm
 {                                 // parenthesis mark the main programm
   printf("Hallo Welt!\n");        // output in the terminal of the String "Hallo Welt"
 
-  return 0;                       // terminate the programm and return an integer with the value 0 
+  unsigned char char_var = 0;
+  printf("Value of char_var %d \n", char_var);
+
+  char_var = 255;
+  printf("Value of char_var %d \n", char_var);
+
+  char_var = 256;
+  printf("Value of char_var %d \n", char_var);
+
+  char_var = 257;
+  printf("Value of char_var %d \n", char_var);
+
+  return 0;                       // terminate the programm and return an integer with the value 0
 }
