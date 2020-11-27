3a4df3146f64aa7f05a0d9f49c7d5f3c1be11108
Christian Pfitzner <christian.pfitzner@evocortex.com>
2020-11-27 11:21:20 +0100

    Update for pointers

---
+++ 05_Zeiger/110_zeiger_call_by_reference.c
@@ -0,0 +1,35 @@
+
+
+// Versuch die Variablen zu vertauschen
+void swap1(int a, int b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+
+
+// Vertauschen der Zeiger auf Variablen
+void swap2(int *a, int *b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+
+int main(void)
+{
+    int x=4, y=7;
+    printf("x: %d, y: %d\n", x, y);
+
+
+    swap1( x, y);
+    printf("x: %d, y: %d\n", x, y);
+
+    swap2(&x, &y);
+    printf("x: %d, y: %d" , x, y);
+
+
+    return 0;
+}
--- temp.c
+++ temp.c
@@ -1,115 +1,51 @@
-/*---------------------------------------------------------------------------
-* Technische Hochschule Ingolstadt
-* Fakultï¿½t Elektrotchnik und Informatik - Elektrotechnik und Elektromobilitï¿½t
-*----------------------------------------------------------------------------
-* Thema: Vorfï¿½hraufgabe bruch.c
-* Autor: Bauer Lukas
-* Datum: 15.11.2020
-*----------------------------------------------------------------------------
-* Modulbeschreibung:
-* Das Programm liest zwei rationale Zahlen als Bruch ein, verrechnet diese
-* mit einer gewï¿½nschten Rechenoperation anschlieï¿½end gibt das Programm
-* die Lï¿½sung als ungegï¿½rzten Bruch und als Dezimalzahl mit zwei
-* Nachkommastellen aus.
-*/
+
+
 
 #include <stdio.h>
+
+//
+//// Druckt ein einzelnes Zeichen in das Terminal
+//// Input Parameter:
+////     char         Einzelnes Ascii Zeichen, was ausgegeben wird
+//void drucke(char);
+//
+//// Macht einen Ton in Windows
+//void beep(void);
+//
+//// Funktion, welche die Zahl zwei zurückgibt
+//int return_2(void);
+
+
+void drucke(char c) { printf("%c", c); }
+
+void beep (void)    { printf("\a"); }
+
+int return_2(void)  { return 2; }
+
+
+
+
+
+
+
+
+
 
 
 int main(void)
 {
-    int     z1;      //Zï¿½hler 1                                                         //Initialisierung und Beschreibung der Variablen
-    int     n1;      //Nenner 1
-    int     z2;      //Zï¿½hler 2
-    int     n2;      //Nenner 2
-    char    r;       //Rechenoperator
-    float   e;       //Ergebnis Flieï¿½komma ,00
-    int     ez;      //Ergebnis Zï¿½hler
-    int     en;      //Ergebnis Nenner
-
-    printf  ("Geben Sie den Zaehler des ersten Bruchs ein: ");                          //Einlesen Bruch1
-    scanf   ("%i", & z1);
-    printf  ("Geben Sie den dazugehoerigen Nenner ein: ");
-    scanf   ("%i", & n1);
-
-    fflush  (stdin);
-
-    printf  ("Geben Sie nun den Zaehler des zweiten Bruchs ein: ");                     //Einlesen Bruch2
-    scanf   ("%i", & z2);
-    printf  ("Geben Sie den dazugehoerigen Nenner ein: ");
-    scanf   ("%i", & n2);
-
-    fflush  (stdin);
-
-    printf  ("Welche Rechenoperation (+, -, *, /,) wollen Sie durchfuehren? ");         //Einlesen des Rechenzeichens
-    scanf   ("%c", & r);
-
-    if (n2 == 0 || n1 == 0)                                                             //Fehlerï¿½berprï¿½fung
-    {
-        printf ("Nenner darf nicht 0 sein!");                                           //Fehlerï¿½berprï¿½fung ob 0 im Nenner
-        return 0;
-    }
-
-    if (r=='+');                                                                        //Fehlerï¿½berprï¿½fung auf gï¿½ltiges Rechenzeichen
-    else if (r=='-');
-    else if (r=='*');
-    else if (r=='/');
-    else
-    {
-        printf ("\nRechenzeichen ungueltig!");
-        return 0;
-    }
-
-
-    printf ("\nIhre Rechnung lautet: %i|%i %c %i|%i =\n\n", z1, n1, r, z2, n2);         //Ausgabe der Rechnung
-
-    printf ("Das Ergebnis lautet:");                                                    //Beginn Ergebnisausgabe
-
-    if (r =='+')                                                                        //Addition zweier Brï¿½che
-    {
-        if (n1==n2)                                                                     //Addition bei gleichem Nenner
-        {
-            printf (" %i|%i\n", ez= z1+ z2, en=n1);                                     //Ergebnisausgabe
-            printf ("Gekuerztes Ergebnis: %.2f\n", e= (float)ez/ (float)en);            //Ergebnis 2 Nachkommastellen
-        }
-        else                                                                            //Addition Nenner ungleich
-        {
-            int gn= n1* n2;
-            z1= gn/ n1* z1;
-            z2= gn/ n2* z2;
-            printf (" %i|%i\n", ez= z1+ z2, en=gn);                                     //Ergebnisausgabe
-            printf ("Gekuerztes Ergebnis: %.2f\n", e= (float)ez/ (float)en);            //Ergebnis 2 Nachkommastellen
-        }
-    }
-
-    if (r=='-')                                                                         //Subtraktion zweier Brï¿½che
-    {
-         if (n1==n2)                                                                    //Subtraktion bei gleichem Nenner
-        {
-            printf (" %i|%i\n", ez= z1- z2, en=n1);                                     //Ergebnisausgabe
-            printf ("Gekuerztes Ergebnis: %.2f\n", e= (float)ez/ (float)en);            //Ergebnis 2 Nachkommastellen
-        }
-        else                                                                            //Subtraktion Nenner ungleich
-        {
-            int gn= n1* n2;
-            z1= gn/ n1* z1;
-            z2= gn/ n2* z2;
-            printf (" %i|%i\n", ez= z1- z2, en=gn);                                     //Ergebnisausgabe
-            printf ("Gekuerztes Ergebnis: %.2f\n", e= (float)ez/ (float)en);            //Ergebnis 2 Nachkommastellen
-        }
-    }
-
-    if (r=='*')                                                                         //Multiplikation zweier Brï¿½che
-    {
-        printf (" %i|%i\n", ez= z1* z2, en= n1* n2);                                    //Ergebnisausgabe
-        printf ("Gekuerztes Ergebnis: %.2f\n", e= (float)ez/ (float)en);                //Ergebnis 2 Nachkommastellen
-    }
-
-    if (r=='/')                                                                         //Division zweier Brï¿½che
-    {
-        printf ("%i|%i\n", ez= z1* n2, en= n1* z2);                                     //Ergebnisausgabe
-        printf ("Gekuerztes Ergebnis: %.2f\n", e= (float)ez/ (float)en);                //Ergebnis 2 Nachkommastellen
-    }
-
+    drucke('@');
+    beep (); // Die Klammern sind notwendig!
+    printf("%d", return_2());
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
