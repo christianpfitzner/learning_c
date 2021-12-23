0644175ab3075f231a9f36bef5a2a880d74ec312
Christian Pfitzner <christian.pfitzner@evocortex.com>
2021-12-23 09:36:24 +0100

    Update for lecture.

--- 99_Praktikum/liste_praktikum.c
+++ 99_Praktikum/liste_praktikum.c
@@ -94,16 +94,15 @@ void printHochschule(Student *s) // Folie 202
 
 
 // Method zum Erzeugen von Studierenden
-
 Student *createStudent(String name, int nr)
 {
-    Student *s = malloc(sizeof(Student));
+    Student *s = malloc(sizeof(Student));   // Reservieren von Speicher für Student
 
-    s->name = malloc(strlen(name) + 1);
-    strcpy(s->name, name);
+    s->name = malloc(strlen(name) + 1);     // Reservieren von Speicher für den Namen
+    strcpy(s->name, name);                  // Zuweisen des Namen
 
-    s->matrikelnr = nr;
-    s->next = NULL;
+    s->matrikelnr = nr;                     // Zuweisung der Matrikelnummer
+    s->next       = NULL;                   // Zuweisung des nächsten Studierenden
 
     return s;
 }
@@ -121,14 +120,34 @@ void push_front(Student **liste, Student *s)
 
 void push_back(Student **liste, Student *s)
 {
-
-
+    while(*liste != NULL)
+    {
+        liste = &(*liste)->next;
+    }
+    *liste = s;
 }
 
 void delete_element(Student **liste, Student *s)
 {
+    while(*liste != NULL && !equals(*liste, s))
+    {
+        liste = &(*liste)->next;
+    }
+    if(*liste != NULL)
+    {
+        s      = *liste;
+        *liste = (*liste)->next;
 
-
+        printf("****************\n");
+        printf("Entfernt: \n");
+        printStudent(s);
+        free(s);
+        printf("****************\n");
+    }
+    else
+    {
+        printf("Eintrag nicht gefunden\n");
+    }
 }
 
 
@@ -143,6 +162,9 @@ int main(void)
     push_front(&hochschule, createStudent("Bob",   100002) );
     push_front(&hochschule, createStudent("Anna",  100001) );
     push_front(&hochschule, createStudent("Bob",   100005) );
+
+    delete_element(&hochschule, createStudent("Peter", 100009));
+
 
     printHochschule(hochschule);
 
